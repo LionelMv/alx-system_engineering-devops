c7f06e105a216bc555fdf35edb137b7bbcd8776f
LionelMv <mvangicode@gmail.com>
2023-07-12 20:11:31 +0300

    new config for Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,57 +1,55 @@
-# Install Nginx web server (w/ Puppet)
-
-include apt::update
+# Install Nginx web server (with Puppet)
 
 package { 'nginx':
-  ensure => 'installed',
-}
-
-file { '/var/www':
-  ensure => directory,
-  mode   => '0755',
-}
-
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    error_page 404 /404.html;
-    location /404.html {
-        internal;
-    }
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}
-",
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  notify  => Service['nginx'],
+  ensure => installed,
 }
 
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  require   => [Package['nginx'], File['/etc/nginx/sites-enabled/default']],
+  ensure => running,
+  enable => true,
 }
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => 'Ceci n\'est pas une page.',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location = / {
+        echo "Hello World!";
+    }
+
+    location /redirect_me {
+        return 301 http://example.com/destination_page;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+}
+',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
