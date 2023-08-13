6f3db6647b01d967194c8f318876e25c4e9182ce
LionelMv <mvangicode@gmail.com>
2023-08-14 01:52:25 +0300

    catering for subdirect1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,17 +8,15 @@ for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="tmp"):
+def recurse(subreddit, hot_list=[], after="None"):
     """
     Return all hot articles for a given subreddit
     Return None if invalid subreddit given
     """
     # Set a custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': 'ubuntu_api_advanced_v1.0'}
-
-    # Make the API request
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    if after != "tmp":
+    if after:
         url += f"?after={after}"
 
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -28,8 +26,6 @@ def recurse(subreddit, hot_list=[], after="tmp"):
 
     data = response.json()
     results = data['data']['children']
-    if not results:
-        return hot_list
 
     for entry in results:
         # hot_list.append(entry.get('data').get('title'))
@@ -38,5 +34,4 @@ def recurse(subreddit, hot_list=[], after="tmp"):
     next_after = data.get('data').get('after')
     if not next_after:
         return hot_list
-
     return recurse(subreddit, hot_list, next_after)
